90a92875ba10043e78727a6aeb81412058d4e7c3
Kirbitz <tyler.marefke@gmail.com>
2022-11-08 12:50:09 -0600

    Quick changes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const { DummyFunction } = require('./router.js')
 
 const app = new Express()
 
+app.use(Express.json())
+
 // Logs all request made to the server
 app.use((req, response, next) => {
   console.log(`${req.method} at ${req.path}`)
@@ -20,5 +22,9 @@ app.get('/update', (req, res) => {
 app.use(Express.static('public'))
 
 app.post('/fun', DummyFunction)
+
+app.get('/*', (req, res) => {
+  res.status(404).json({ error: 'No Page Found' })
+})
 
 module.exports = app
--- server/router.js
+++ server/router.js
@@ -1,11 +1,17 @@
 const DummyFunction = (req, res) => {
-  const data = module.exports.myCoolFunction(req.body)
-
-  res.status(200).json(data)
+  console.log(req.body)
+  if(Object.keys(req.body).length > 0){
+    const data = module.exports.MoodFunction(req.body.inputValue)
+    res.status(200).json(data)
+  } else {
+    res.status(400).json({
+      error: 'Please pass a value to the input field'
+    })
+  }
 }
 
-const MoodFunction = (data) => {
-  if(data === 'happy'){
+const MoodFunction = (inputVal) => {
+  if(inputVal === 'happy'){
     return { mood: 'sadness' }
   } else {
     return { mood: 'happy' }
